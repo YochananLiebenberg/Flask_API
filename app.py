2af99827c2a52cf1baf8c97c8bf9fb56e0f1836b
YochananLiebenberg <yochananliebenberg@gmail.com>
2022-04-08 08:20:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,11 @@
 from flask import Flask
 
-import pymongo  # package for working with MongoDB
-
-import json
-
 app = Flask(__name__)
-
-
-class create_dict(dict):
-    # __init__ function
-    def __init__(self):
-        self = dict()
-        # Function to add key:value
-
-    def add(self, key, value):
-        self[key] = value
 
 
 @app.route('/')
 def hello_world():
     return "Hello, World!"
-
-
-# Create endpoint: Events
-@app.route('/events')
-def events():
-    # GET EVENTS API:
-    # Connect to MongoDB
-    event = pymongo.MongoClient("mongodb://100.67.53.133:27017/")
-    db = event["eventsdb"]
-    events = db["events"]
-
-    events_list = []
-
-    for event in events.find():
-        mydict = create_dict()
-        mydict.add("id", str(event['_id']))
-        mydict.add("title", event['title'])
-        mydict.add("images", event['images'])
-        mydict.add("time", event['time'])
-        mydict.add("categoryId", event['categoryId'])
-        mydict.add("userId", event['userId'])
-        mydict.add("location", event['location'])
-
-        events_list.append(mydict)
-
-    return json.dumps(events_list), 200  # return data and 200 OK code
 
 
 if __name__ == "__main__":
