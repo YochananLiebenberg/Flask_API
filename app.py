0c19b4cc102f92ef7b0d85cebb6cb9686d96f3c6
YochananLiebenberg <yochananliebenberg@gmail.com>
2022-04-08 05:46:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify, request
+app=Flask(__name__)
 
-@app.route("/hello")
-def hello():
-    return {'message': "Hello world!"}, 200
-
+@app.route("/addseven",methods=['GET','POST'])
+def index():
+    if request.method=='GET':
+          
+    number = request.args.get('number')
+    result = number + 7
+    return jsonify(result)
+    else:
+        return jsonify({'Error':"This is a GET API method"})
+    
 if __name__ == '__main__':
-    app.run(
-
-    ) 
+    app.run(debug=True,host='0.0.0.0', port=9007)
