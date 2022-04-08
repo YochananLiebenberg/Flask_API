818642e10be0ae9dfe4e86ed5ceb0fc9f615963f
YochananLiebenberg <yochananliebenberg@gmail.com>
2022-04-08 08:03:29 +0100

    Update

--- app.py
+++ app.py
@@ -1,61 +1,52 @@
-import subprocess
-import sys
-
 from flask import Flask
-
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'flask-restful'])
-from flask_restful import Resource, Api, reqparse
 
 import pymongo  # package for working with MongoDB
 
 import json
 
 app = Flask(__name__)
-api = Api(app)
 
 
 class create_dict(dict):
     # __init__ function
     def __init__(self):
         self = dict()
-
         # Function to add key:value
 
     def add(self, key, value):
         self[key] = value
 
 
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
+
+
 # Create endpoint: Events
-class Events(Resource):
-    def get(self):
-        # GET EVENTS API:
-        # Connect to MongoDB
-        event = pymongo.MongoClient("mongodb://localhost:27017/")
-        db = event["eventsdb"]
-        events = db["events"]
+@app.route('/events')
+def events():
+    # GET EVENTS API:
+    # Connect to MongoDB
+    event = pymongo.MongoClient("mongodb://100.67.53.133:27017/")
+    db = event["eventsdb"]
+    events = db["events"]
 
-        events_list = []
+    events_list = []
 
-        for event in events.find():
-            mydict = create_dict()
-            mydict.add("id", str(event['_id']))
-            mydict.add("title", event['title'])
-            mydict.add("images", event['images'])
-            mydict.add("time", event['time'])
-            mydict.add("categoryId", event['categoryId'])
-            mydict.add("userId", event['userId'])
-            mydict.add("location", event['location'])
+    for event in events.find():
+        mydict = create_dict()
+        mydict.add("id", str(event['_id']))
+        mydict.add("title", event['title'])
+        mydict.add("images", event['images'])
+        mydict.add("time", event['time'])
+        mydict.add("categoryId", event['categoryId'])
+        mydict.add("userId", event['userId'])
+        mydict.add("location", event['location'])
 
-            events_list.update(mydict)
+        events_list.append(mydict)
 
-        return json.dumps(events_list), 200  # return data and 200 OK code
-
-    pass
+    return json.dumps(events_list), 200  # return data and 200 OK code
 
 
-api.add_resource(Events, '/events')  # and '/movies' is our entry point for Movies
-
-if __name__ == '__main__':
-    app.run(
-
-    )  # run our Flask app
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
